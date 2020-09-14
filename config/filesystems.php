6144ff53756d0fbbaf62f137a436c433b2c8de45
jaaf <jaaf@fixe.home>
2020-09-14 09:42:32 +0200

    changing public file driver to public

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,7 +49,7 @@ return [
         ],
 
         'public' => [
-            'driver' => 'local',
+            'driver' => 'public',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             /*ajouté */
