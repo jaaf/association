bf3faee373d9a96df28ababf23c3458c46c81c13
jaaf <jaaf@fixe.home>
2020-10-01 14:47:54 +0200

    adding adherents to infoletter supplément

--- resources/views/emails/inc/intro.blade.php
+++ resources/views/emails/inc/intro.blade.php
@@ -1,6 +1,6 @@
 <div style="color: rgb(224, 111, 11); font-style: italic;font-size: 1em;">
-    <p>Vous recevez ce courriel parce que vous faites partie des membres inscrits sur le site
-     de l'association Denentzat ou parce que vous avez demandé l'ouverture d'un compte.</p><p>
+    <p>Vous recevez ce courriel soit parce que vous faites partie des membres inscrits sur le site
+     de l'association Denentzat, soit parce que vous avez demandé l'ouverture d'un compte, soit parce que vous êtes adhérent.</p><p>
     Ce courriel vous est adressé automatiquement par le site de Denentzat. N'y répondez pas,
      votre réponse n'aboutirait nulle part. Si le courriel 
     vous demande une réponse, il vous précise où adresser cette réponse.</p>
